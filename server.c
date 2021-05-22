960f0a9bb9936599195c0839ba2bb333ce700cf2
Abner Espinoza <49922254+abner-espinoza@users.noreply.github.com>
2021-05-21 23:15:03 -0700

    Update server.c

--- server.c
+++ server.c
@@ -2,8 +2,10 @@
 #include "auction.h"
 #include <string.h>
 #include <stdio.h>
+#include <pthread.h>
+#include <signal.h>
 
-void printInstruction(){
+void printInstructions(){
     printf("./bin/zbid_server [-h] [-j N] [-t M] PORT_NUMBER AUCTION_FILENAME.\n\n");
     printf("-h                  Displays this help menu, and returns EXIT_SUCCESS.\n");
     printf("-j N                Number of job threads. If option not specified, default to 2.\n");
@@ -12,89 +14,96 @@ void printInstruction(){
     printf("AUCTION_FILENAME    File to read auction item information from at the start of the server.\n");
 }
 
+// shared resources
+int auctionID = 1; 	// increment by one after using it  
+List_t* users; 		// list of users, each user being a struct
+List_t* auctions; 	// list of auctions, each auction being a struct
+List_t* jobQueue; 	// list of jobs, each 
+
 int ID_for_new;
 List_t* user_list;
 List_t* aution_list;
-List_t* job_queue;
+List_t* job_queue;	// job buffer
 int num_job_thread=2;
 int tick_second=0;
 int server_port=-1;
 char* auction_file_name=NULL;
 
+
 int main(int argc, char* argv[]) {
-    //////////////////////////////////COLLECTING INFO FROM COMMAND INPUT//////////////////////////////
-        if(argc<3){
+    //////////////////////////////////COLLECTING INFO FROM COMMAND INPUT////////////////////////////// THIEN
+        if (argc < 3){
             printInstruction();
-            if(argc==2&&strcmp(argv[1],"-h")==0)return EXIT_SUCCESS;
+            if (argc == 2 && strcmp(argv[1], "-h") == 0)
+              	return EXIT_SUCCESS;
             return EXIT_FAILURE;
         }
-        auction_file_name=argv[argc-1];
-        server_port=atoi(argv[argc-2]);
+  
+        auction_file_name = argv[argc-1];
+        server_port = atoi(argv[argc-2]);
+  
         int iter;
-        for(iter=1;iter<argc-2;iter++){
-            if(strcmp(argv[iter],"-h")==0){
+        for(iter = 1; iter < argc-2; iter++) {
+            if (strcmp(argv[iter], "-h") == 0) {
                 printInstruction();
                 return EXIT_SUCCESS;
             }
-            else if(strcmp(argv[iter],"-j")==0){
+            else if (strcmp(argv[iter], "-j") == 0) {
                 iter++;
-                if(iter>=argc-2){
+                if (iter >= argc-2) {
                     printInstruction();
                     return EXIT_FAILURE;
                 }
-                num_job_thread=atoi(argv[iter]);
+                num_job_thread = atoi(argv[iter]);
             }
-            else if(strcmp(argv[iter],"-t")==0){
+            else if (strcmp(argv[iter], "-t") == 0) {
                 iter++;
-                if(iter>=argc-2){
+                if(iter >= argc-2) {
                     printInstruction();
                     return EXIT_FAILURE;
                 }
-                tick_second=atoi(argv[iter]);
+                tick_second = atoi(argv[iter]);
             }
-            else{
+            else {
                 printInstruction();
                 return EXIT_FAILURE;
             }
         }
-        printf("NUM_JOB_THREAD = %d\n",num_job_thread);
-        printf("TICK_SECOND = %d\n",tick_second);
-        printf("PORT_NUMBER = %d\n",server_port);
-        printf("AUCTION_FILENAME = %s\n",auction_file_name);
+  
+  	//////////////////////// SETUP GLOBAL VARIABLE AND PREFILL AUCTION LIST//////////////////////////// ABNER
+  		// if auction_file_name == NULL, ignore
+  		if (auction_file_name != NULL) {
+          	// opens file, prefills auctions list
+          	FILE* fp = fopen(auction_file_name, "r") 
+            if (fp == NULL)
+                return EXIT_FAILURE;
+          	
+          	int i = 1;
+          	char* cur = (char*)malloc(sizeof(char));				// current row in file
+          	auction_t* auc = (auction_t*)malloc(sizeof(auction_t));	// auction information
+          	while (fgets(cur, 100, fp) != EOF) {
+            	if ((i % 4) == 0) {
+                	auc = (auction_t*)malloc(sizeof(auction_t));
+                }
+              	else if (i == 1) {
+                  	strcpy(auc->file_name, cur);
+                }
+              	else if (i == 2) {
+                  	auc->duration = atoi(cur);
+                }
+              	else {
+                  	auc->min_price = atoi(cur);
+              		insertFront(auctions, (void*)auc);
+                }
+              	i++;
+          	}
+          	free(auc);		// freeing last, unused auc
+          	auc = NULL; 	// avoiding future error 
+        }
+  		
 
-
-    ///////////////////////////////////////////RUN SERVER/////////////////////////////////////////////
-    ID_for_new=1;
-    user_list=malloc(sizeof(List_t));
-    aution_list=malloc(sizeof(List_t));
-    job_queue=malloc(sizeof(List_t));
-    // when server is started:
-    //      N job threads are created
-    //      tick thread is created
-    //      create shared resources:
-    //          User
-    //              username
-    //              password
-    //              list of won auctions
-    //              file descriptors
-    //              in reader/writer model
-    //          Auction ID
-    //              shared variable indicating the next available ID starting at 1
-    //              in single mutex model
-    //          Auctions
-    //              auction id
-    //              creator's usernae
-    //              remainig ticks
-    //              set of users watching
-    //              in reader/writer model
-    //          Job queue
-    //              allow:
-    //                  creating auction
-    //                  make a bid
-    //                  view running auctions
-    //              in producer/consumer access model
-    // 3 type of threads:
-    //      1 main thread
+    ///////////////////////////////////////////RUN SERVER///////////////////////////////////////////// BOTH
+  	//      1 main thread
     //          loop to wait for client
     //              check username and password
     //                  if new username
@@ -118,5 +127,6 @@ int main(int argc, char* argv[]) {
     //              look for ended auctions at the end of each tick
     //                  handle when an auction should end by adding to job queue (producer)
     //              is created when server started
-    return 0;
+        
+  	return 0;
 }
